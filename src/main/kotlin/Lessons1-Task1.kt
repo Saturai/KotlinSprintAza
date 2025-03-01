9645e1dce338e7e8c299aeb804db6fb13e745191
saturai <moirai2010@mail.ru>
2025-03-01 11:42:44 +0300

    lessons1Task1 order and message

--- src/main/kotlin/Lessons1-Task1.kt
+++ src/main/kotlin/Lessons1-Task1.kt
@@ -1,7 +1,7 @@
 fun main() {
 
-    val numberOfOders:Int = 75
+    val numbersOfOders:Int = 75
 
-    val thankMessage:String = "Спасибо за заказ. Рады новому сотрудничеству"
+    val thanksMessage:String = "Спасибо за заказ. Рады новому сотрудничеству"
 
 }
