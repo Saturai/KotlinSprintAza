1850acd46d91119eae7de6dcad6f7f34058f7f9b
saturai <moirai2010@mail.ru>
2025-03-01 12:43:11 +0300

    lessons1Task2 Employees -1

---
+++ src/main/kotlin/Lessons1-Task2.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    val numbersOfOders:Int = 75
+
+    val thanksMessage:String = "Спасибо за заказ. Рады новому сотрудничеству"
+
+    println(numbersOfOders)
+    println(thanksMessage)
+
+    var numberOfEmployees:Int = 2000
+    //println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+
+
+}
