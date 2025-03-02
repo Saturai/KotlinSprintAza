5fdaac02c2a9c404b57ccc1c32cd3ff5c6efbd7d
saturai <moirai2010@mail.ru>
2025-03-02 15:08:44 +0300

    lessons1-Task2 Employees -1

--- src/main/kotlin/Lessons1-Task2.kt
+++ src/main/kotlin/Lessons1-Task2.kt
@@ -2,14 +2,14 @@
 
 fun main() {
 
-    val numbersOfOders:Int = 75
+    val numbersOfOders: Int = 75
 
-    val thanksMessage:String = "Спасибо за ваш заказ. Рады новому сотрудничеству!"
+    val thanksMessage: String = "Спасибо за ваш заказ. Рады новому сотрудничеству!"
 
     println("Количество заказов: ${numbersOfOders}")
     println(thanksMessage)
 
-    var numberOfEmployees:Int = 2000
+    var numberOfEmployees: Int = 2000
     //println(numberOfEmployees)
 
     numberOfEmployees = 1999
