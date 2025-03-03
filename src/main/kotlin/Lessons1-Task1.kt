af1553e086aa7cbc8c7562c5688b22ab8ed27f95
saturai <moirai2010@mail.ru>
2025-03-03 21:57:25 +0300

    Lessons2-Task1-average value

--- src/main/kotlin/Lessons1-Task1.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-
-    val numberOfOders:Int = 75
-
-    val thankMessage:String = "Спасибо за заказ. Рады новому сотрудничеству"
-
-}
---
+++ src/main/kotlin/Lessons2-Task1-average value.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val student1 = 3
+    val student2 = 4
+    val student3 = 3
+    val student4 = 5
+
+    val averageScore: Float = (student1+student2+student3+student4) / 4F
+
+    println(averageScore)
+
+}
