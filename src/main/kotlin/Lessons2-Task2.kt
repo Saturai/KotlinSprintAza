d7e1a3b54a7a8d56614a93e886174ebe250c6bb4
saturai <moirai2010@mail.ru>
2025-03-04 23:09:42 +0300

    Lessons2-Task2 created a budget

---
+++ src/main/kotlin/Lessons2-Task2.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+   val numberOfEmployees = 50
+   val numberOfInterns = 30
+   val totalEmployees = numberOfEmployees + numberOfInterns
+
+   val salaryOfOfEmployees = 30000
+   val salaryOfInterns = 20000
+
+   val budgetForPermanentEmployees = numberOfEmployees * salaryOfOfEmployees
+   val budgetForPermanentInterns = numberOfInterns * salaryOfInterns
+
+   val totalBudget = budgetForPermanentInterns + budgetForPermanentInterns
+   val averageSalary = totalBudget / totalEmployees
+
+}
